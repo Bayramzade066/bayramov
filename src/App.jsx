403243e103d838ff6067f2dff639ecbf7cee9e39
Bayramzade066 <bayramzade.313@gmail.com>
2024-01-22 23:12:23 +0400

    fix mobile display  and other edits

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import { useState } from "react";
 import "./App.css";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
+import mp3 from "./files/getLucky.mp3"
+
 
 function App() {
   const [section, setSection] = useState(0);
+  const [animateOn, setAnimateOn] = useState(false);
+  const [animateOff, setAnimateOff] = useState('transition-all opacity-0 transition-all opacity-1');
+  const [off,setOff] = useState(false)
+  const [on,setOn] = useState(false)
 
+  
   return (
     <>
       <div className="hidden sm:!block bg"></div>
-      <div className=" flex items-center justify-center bg-emerald-900">
-        <div className=" w-full h-full sm:min-w-[1100px] sm:min-h-[650px] sm:w-[60%] sm:h-[60%] p-5 rounded-lg bg-zinc-700 shadow-xl block sm:fixed">
+      <div className="w-full min-h-screen sm:h-full bg-zinc-800 flex  items-start sm:items-center sm:justify-center sm:bg-emerald-900">
+        <div className=" w-full h-full sm:min-w-[1000px] sm:min-h-[550px] sm:w-[60%] sm:h-[60%] p-5 rounded-lg sm:bg-zinc-800 sm:shadow-xl block sm:fixed">
           {/* navbar */}
 
           <div className=" flex flex-col sm:flex-row h-[150px] sm:h-[30px] text-center px-5 gap-3 sm:gap-0 ">
@@ -69,7 +76,7 @@ function App() {
               </span>
             </div>
             <span
-              className="text-xl sm:text-lg bg-green-900  hover:bg-green-700 rounded-lg text-neutral-300 font-bold hover:text-neutral-600 transition-all hover:transition-all cursor-pointer w-auto pt-3 sm:pt-[4px] py-3 px-4"
+              className="text-xl sm:text-lg bg-green-900  hover:bg-green-700 rounded-lg text-neutral-300 font-bold hover:text-neutral-900 transition-all hover:transition-all cursor-pointer w-auto pt-3 sm:pt-[1px] py-3 px-4"
               onClick={() => {
                 setSection(4);
               }}
@@ -80,7 +87,7 @@ function App() {
 
           {section === 0 && (
             <div className="w-full h-full sm:gap-16 flex flex-col sm:flex-row items-center justify-center">
-              <div className=" w-[80%] h-[25%] my-10 sm:my-0 sm:w-[40%] sm:h-[80%] sm:w-1/2  glitch shadow-2xl rounded-lg bg-cover bg-no-repeat bg-center w-full"></div>
+              <div className=" w-[300px] h-[150px] my-8 sm:my-0 sm:w-[40%] sm:h-[80%] sm:w-1/2  glitch sm:shadow-2xl rounded-lg bg-cover bg-no-repeat bg-center w-full"></div>
               <div className="sm:w-1/2 h-[80%] flex flex-col justify-start sm:justify-center gap-5">
                 <h1 className="max-w-[340px] tracking-wider animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-3xl text-neutral-300 font-bold h-[30px] sm:min-h-[30px] sm:h-[75px]">
                   Bayramov Muhammed
@@ -99,7 +106,7 @@ function App() {
                   <span className="h-6 text-sm sm:text-lg text-neutral-500 w-full mb-2 ">
                     Play the music and look at the site.. :)
                   </span>
-                  <AudioPlayer src="/getLucky.mp3" volume={0.3} />
+                  <AudioPlayer src={mp3} volume={0.3} />
                 </div>
               </div>
             </div>
@@ -133,15 +140,15 @@ function App() {
                   <span className="text-md sm:text-md tracking-wide text-neutral-200 font-bold w-auto h-auto mb-5 sm:mb-0">
                     - Hobbie:
                     <span className="text-green-600">
-                      camping,chess,literature and software books
+                    camping,chess,literature and software books
                     </span>
                     <br /> <br />- Address:
                     <span className="text-green-600">Azerbaijan, Baku</span>
                     <br /> <br />- age:
                     <span className="text-green-600">22</span> <br /> <br />-
-                    Work:
+                    Work: 
                     <span className="text-green-600">
-                      Azerbaijan medical university
+                      Azerbaijan medical university <br/> & Freelancer
                     </span>
                   </span>
                 </div>
@@ -158,73 +165,73 @@ function App() {
                 <div className="w-full flex flex-col h-[60%] sm:h-full gap-10 sm:gap-10">
                   <div className="w-full flex flex-col sm:flex-row items-start justify-center gap-10 sm:gap-20">
                     <div className="flex items-center justify-center rounded-lg h-[25px] w-full sm:w-[35%]  block">
-                      <span className="text-green-600 text-lg w-full font-bold">
+                      <span className="text-green-600 text-md  w-full font-bold">
                         Javascript -
                       </span>
-                      <div className="w-full h-4 bg-neutral-300 rounded-full">
-                        <div className="w-[80%] h-full text-center text-xs text-white bg-emerald-900 rounded-full"></div>
+                      <div className="w-full h-4 bg-neutral-300 border border-emerald-900 rounded-full">
+                        <div className="w-[80%] h-full border border-emerald-900 bg-emerald-900 rounded-full"></div>
                       </div>
                     </div>
                     <div className="flex items-center justify-center rounded-lg h-[25px] w-full sm:w-[35%]  block">
-                      <span className="text-green-600 text-lg w-full font-bold">
+                      <span className="text-green-600 text-md  w-full font-bold">
                         React.js -
                       </span>
-                      <div className="w-full h-4 bg-neutral-300 rounded-full">
-                        <div className="w-[85%] h-full text-center text-xs text-white bg-emerald-900 rounded-full"></div>
+                      <div className="w-full h-4 bg-neutral-300 border border-emerald-900 rounded-full">
+                        <div className="w-[85%] h-full border border-emerald-900 bg-emerald-900 rounded-full"></div>
                       </div>
                     </div>
                   </div>
                   <div className="w-full flex flex-col sm:flex-row items-start justify-center gap-10 sm:gap-20">
                     <div className="flex items-center justify-center rounded-lg h-[25px] w-full sm:w-[35%]  block">
-                      <span className="text-green-600 text-lg w-full font-bold">
+                      <span className="text-green-600 text-md  w-full font-bold">
                         Firebase -
                       </span>
-                      <div className="w-full h-4 bg-neutral-300 rounded-full">
-                        <div className="w-[65%] h-full text-center text-xs text-white bg-emerald-900 rounded-full"></div>
+                      <div className="w-full h-4 bg-neutral-300 border border-emerald-900 rounded-full">
+                        <div className="w-[65%] h-full border border-emerald-900 bg-emerald-900 rounded-full"></div>
                       </div>
                     </div>
                     <div className="flex items-center justify-center rounded-lg h-[25px] w-full sm:w-[35%]  block">
-                      <span className="text-green-600 text-lg w-full font-bold">
+                      <span className="text-green-600 text-md  w-full font-bold">
                         Github & Git -
                       </span>
-                      <div className="w-full h-4 bg-neutral-300 rounded-full">
-                        <div className="w-[70%] h-full text-center text-xs text-white bg-emerald-900 rounded-full"></div>
+                      <div className="w-full h-4 bg-neutral-300 border border-emerald-900 rounded-full">
+                        <div className="w-[70%] h-full border border-emerald-900 bg-emerald-900 rounded-full"></div>
                       </div>
                     </div>
                   </div>
                   <div className="w-full flex flex-col sm:flex-row items-start justify-center gap-10 sm:gap-20">
                     <div className="flex items-center justify-center rounded-lg h-[25px] w-full sm:w-[35%]  block">
-                      <span className="text-green-600 text-lg w-full font-bold">
+                      <span className="text-green-600 text-md  w-full font-bold">
                         Wordpress -
                       </span>
-                      <div className="w-full h-4 bg-neutral-300 rounded-full">
-                        <div className="w-[55%] h-full text-center text-xs text-white bg-emerald-900 rounded-full"></div>
+                      <div className="w-full h-4 bg-neutral-300 border border-emerald-900 rounded-full">
+                        <div className="w-[55%] h-full border border-emerald-900 bg-emerald-900 rounded-full"></div>
                       </div>
                     </div>
                     <div className="flex items-center justify-center rounded-lg h-[25px] w-full sm:w-[35%]  block">
-                      <span className="text-green-600 text-lg w-full font-bold">
+                      <span className="text-green-600 text-md  w-full font-bold">
                         Kotlin -
                       </span>
-                      <div className="w-full h-4 bg-neutral-300 rounded-full">
-                        <div className="w-[30%] h-full text-center text-xs text-white bg-emerald-900 rounded-full"></div>
+                      <div className="w-full h-4 bg-neutral-300 border border-emerald-900 rounded-full">
+                        <div className="w-[30%] h-full border border-emerald-900 bg-emerald-900 rounded-full"></div>
                       </div>
                     </div>
                   </div>
                   <div className="w-full flex flex-col sm:flex-row items-start justify-center gap-10 sm:gap-20">
                     <div className="flex items-center justify-center rounded-lg h-[25px] w-full sm:w-[35%]  block">
-                      <span className="text-green-600 text-lg w-full font-bold">
-                        Tailwind & SCSS -
+                      <span className="text-green-600 text-md  w-full font-bold">
+                        Tailwind & SCSS-
                       </span>
-                      <div className="w-full h-4 bg-neutral-300 rounded-full">
-                        <div className="w-[90%] h-full text-center text-xs text-white bg-emerald-900 rounded-full"></div>
+                      <div className="w-full h-4 bg-neutral-300 border border-emerald-900 rounded-full">
+                        <div className="w-[90%] h-full border border-emerald-900 bg-emerald-900 rounded-full"></div>
                       </div>
                     </div>
                     <div className="flex items-center justify-center rounded-lg h-[25px] w-full sm:w-[35%]  block">
-                      <span className="text-green-600 text-lg w-full font-bold">
+                      <span className="text-green-600 text-md  w-full font-bold">
                         API -
                       </span>
-                      <div className="w-full h-4 bg-neutral-300 rounded-full">
-                        <div className="w-[65%] h-full text-center text-xs text-white bg-emerald-900 rounded-full"></div>
+                      <div className="w-full h-4 border border-emerald-900 bg-neutral-300 rounded-full">
+                        <div className="w-[65%] h-full border border-emerald-900  bg-emerald-900 rounded-full"></div>
                       </div>
                     </div>
                   </div>
@@ -235,19 +242,21 @@ function App() {
           {section === 3 && (
             <div className="w-full h-full sm:gap-16 flex flex-col sm:flex-row items-center justify-center sm:px-5">
               <div className="sm:w-1/2 h-[50%] sm:h-[80%] flex flex-col items-start justify-center sm:justify-center gap-5 border-b sm:border-b-0 sm:border-r border-emerald-800 py-5 sm:py-0">
-                <span className="text-xl sm:text-2xl tracking-wide text-neutral-300 font-bold w-auto h-auto pt-20 sm:pt-0 mb-2 sm:mb-0">
+                <span className="text-xl sm:text-2xl tracking-wide text-neutral-300 font-bold w-full h-auto pt-4 sm:pt-0 mb-2 sm:mb-0">
                   Work experience
                 </span>
                 <div className="">
                   <span className="text-md sm:text-md tracking-wide text-neutral-200 font-bold w-auto h-auto mb-5 sm:mb-0">
                     - Pragmatech education center: <br /> Front end menthor -
-                    <span className="text-green-600">2021 / 2022</span> <br />
+                    <span className="text-green-600"> 2021 / 2022</span> <br />
                     <br />
                     - Remox Inc: <br /> Front-end Developer -
                     <span className="text-green-600"> 2022 </span> <br /> <br />
                     - Azerbaijan Medical University: <br /> Technical Support &
                     Developer -
-                    <span className="text-green-600">2022 / Now</span>
+                    <span className="text-green-600"> 2022 / Now</span> <br /> <br />
+                    - Yenikimi MMC: <br /> Freelance & Front-end Developer -
+                    <span className="text-green-600"> 2023 / now </span> 
                   </span>
                 </div>
               </div>
@@ -262,51 +271,51 @@ function App() {
                       href="https://pragmatech-website.github.io/Pragmatech/pragmatech%20design/templates/index.html"
                       className="text-green-600 border-b border-green-600"
                     >
-                      Pragmatech education center's web site (Real project)
+                      Pragmatech education center's web site
                     </a>
-                    <br /> <br />- <a
+                    <br />- <a
                       target="_blank"
                       href="https://bayramzade066.github.io/portfolio-react/"
                       className="text-green-600 border-b border-green-600"
                     >
                       React snow project
                     </a>
-                    <br /> <br />- <a
+                    <br />- <a
                       target="_blank"
                       href="https://demo.remox.io/"
                       className="text-green-600 border-b border-green-600"
                     >
-                      Remox Demo (Real project)
+                      Remox Demo 
                     </a>
-                    <br /> <br />- <a
+                    <br />- <a
                       target="_blank"
                       href="https://drjavadli.com"
                       className="text-green-600 border-b border-green-600"
                     >
                       Dr personal website (Wordpress)
                     </a>
-                    <br /> <br />- <a
+                    <br />- <a
                       target="_blank"
                       href="https://yenikimi.az"
                       className="text-green-600 border-b border-green-600"
                     >
-                      'Yenikimi' Project (Real project)
+                      'Yenikimi' Project 
                     </a>
-                    <br /> <br />- <a
+                    <br />- <a
                       target="_blank"
                       href="https://blog-b19a3.web.app/"
                       className="text-green-600 border-b border-green-600"
                     >
                       Blog website
                     </a>
-                    <br /> <br />- <a
+                    <br />- <a
                       target="_blank"
                       href="https://bayramzade066.github.io/Filters/template/index.html"
                       className="text-green-600 border-b border-green-600"
                     >
                       Filters
                     </a>
-                    <br /> <br />- <a
+                    <br />- <a
                       target="_blank"
                       href="https://bayramzade066.github.io/xogame/"
                       className="text-green-600 border-b border-green-600"
@@ -314,14 +323,14 @@ function App() {
                       
                       Tic Tac Toe
                     </a>
-                    <br /> <br />- <a
+                    <br /> - <a
                       target="_blank"
                       href="https://bayramzade066.github.io/StickyNotes/StickyNotes/index.html"
                       className="text-green-600 border-b border-green-600"
                     >
                       Sticky Notes
                     </a>
-                    <br /> <br />
+                    <br />
                   </span>
                 </div>
               </div>
